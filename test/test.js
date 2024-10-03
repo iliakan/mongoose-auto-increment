be1b3f3a9c05280d71385eae7793f1628eb1c85d
Ilya Kantor <iliakan@users.noreply.github.com>
2024-10-03 23:48:03 +0200

    Update to Mongoose 8

--- test/test.js
+++ test/test.js
@@ -1,8 +1,10 @@
-var async = require('async'),
+let async = require('async'),
 should = require('chai').should(),
 mongoose = require('mongoose'),
 autoIncrement = require('..'),
 connection;
+
+// mongoose.set('debug', true);
 
 before(function (done) {
   connection = mongoose.createConnection('mongodb://127.0.0.1/mongoose-auto-increment-test');
@@ -13,11 +15,9 @@ before(function (done) {
   });
 });
 
-after(function (done) {
-  connection.db.dropDatabase(function (err) {
-    if (err) return done(err);
-    connection.close(done);
-  });
+after(async function() {
+  await connection.dropDatabase();
+  connection.close();
 });
 
 afterEach(function (done) {
@@ -29,228 +29,140 @@ afterEach(function (done) {
 
 describe('mongoose-auto-increment', function () {
 
-  it('should increment the _id field on save', function (done) {
+  it('should increment the _id field on save', async function () {
 
     // Arrange
-    var userSchema = new mongoose.Schema({
+    let userSchema = new mongoose.Schema({
       name: String,
       dept: String
     });
     userSchema.plugin(autoIncrement.plugin, 'User');
-    var User = connection.model('User', userSchema),
+
+    let User = connection.model('User', userSchema),
     user1 = new User({ name: 'Charlie', dept: 'Support' }),
     user2 = new User({ name: 'Charlene', dept: 'Marketing' });
 
-    // Act
-    async.series({
-      user1: function (cb) {
-        user1.save(cb);
-      },
-      user2: function (cb) {
-        user2.save(cb);
-      }
-    }, assert);
+    await user1.save();
+    await user2.save();
 
-    // Assert
-    function assert(err, results) {
-      should.not.exist(err);
-      results.user1[0].should.have.property('_id', 0);
-      results.user2[0].should.have.property('_id', 1);
-      done();
-    }
+    user1.should.have.property('_id', 0);
+    user2.should.have.property('_id', 1);
 
   });
 
-  it('should increment the specified field instead (Test 2)', function(done) {
+  it('should increment the specified field instead (Test 2)', async function() {
 
     // Arrange
-    var userSchema = new mongoose.Schema({
+    let userSchema = new mongoose.Schema({
       name: String,
       dept: String
     });
     userSchema.plugin(autoIncrement.plugin, { model: 'User', field: 'userId' });
-    var User = connection.model('User', userSchema),
+    let User = connection.model('User', userSchema),
     user1 = new User({ name: 'Charlie', dept: 'Support' }),
     user2 = new User({ name: 'Charlene', dept: 'Marketing' });
 
-    // Act
-    async.series({
-      user1: function (cb) {
-        user1.save(cb);
-      },
-      user2: function (cb) {
-        user2.save(cb);
-      }
-    }, assert);
+    await user1.save();
+    await user2.save();
 
-    // Assert
-    function assert(err, results) {
-      should.not.exist(err);
-      results.user1[0].should.have.property('userId', 0);
-      results.user2[0].should.have.property('userId', 1);
-      done();
-    }
+    user1.should.have.property('userId', 0);
+    user2.should.have.property('userId', 1);
 
   });
 
 
-  it('should start counting at specified number (Test 3)', function (done) {
+  it('should start counting at specified number (Test 3)', async function() {
 
     // Arrange
-    var userSchema = new mongoose.Schema({
+    let userSchema = new mongoose.Schema({
       name: String,
       dept: String
     });
     userSchema.plugin(autoIncrement.plugin, { model: 'User', startAt: 3 });
-    var User = connection.model('User', userSchema),
+    let User = connection.model('User', userSchema),
     user1 = new User({ name: 'Charlie', dept: 'Support' }),
     user2 = new User({ name: 'Charlene', dept: 'Marketing' });
 
-    // Act
-    async.series({
-      user1: function (cb) {
-        user1.save(cb);
-      },
-      user2: function (cb) {
-        user2.save(cb);
-      }
-    }, assert);
+    await user1.save();
+    await user2.save();
 
-    // Assert
-    function assert(err, results) {
-      should.not.exist(err);
-      results.user1[0].should.have.property('_id', 3);
-      results.user2[0].should.have.property('_id', 4);
-      done();
-    }
+    user1.should.have.property('_id', 3);
+    user2.should.have.property('_id', 4);
 
   });
 
-  it('should increment by the specified amount (Test 4)', function (done) {
+  it('should increment by the specified amount (Test 4)', async function () {
 
     // Arrange
-    var userSchema = new mongoose.Schema({
+    let userSchema = new mongoose.Schema({
       name: String,
       dept: String
     });
 
-    (function() {
-      userSchema.plugin(autoIncrement.plugin);
-    }).should.throw(Error);
-
     userSchema.plugin(autoIncrement.plugin, { model: 'User', incrementBy: 5 });
-    var User = connection.model('User', userSchema),
+    let User = connection.model('User', userSchema),
     user1 = new User({ name: 'Charlie', dept: 'Support' }),
     user2 = new User({ name: 'Charlene', dept: 'Marketing' });
 
+    await user1.save();
+    await user2.save();
 
-
-    // Act
-    async.series({
-      user1: function (cb) {
-        user1.save(cb);
-      },
-      user2: function (cb) {
-        user2.save(cb);
-      }
-    }, assert);
-
-
-    // Assert
-    function assert(err, results) {
-      should.not.exist(err);
-      results.user1[0].should.have.property('_id', 0);
-      results.user2[0].should.have.property('_id', 5);
-      done();
-    }
+    user1.should.have.property('_id', 0);
+    user2.should.have.property('_id', 5);
 
   });
 
 
-
-
   describe('helper function', function () {
 
-    it('nextCount should return the next count for the model and field (Test 5)', function (done) {
+    it('nextCount should return the next count for the model and field (Test 5)', async function() {
 
       // Arrange
-      var userSchema = new mongoose.Schema({
+      let userSchema = new mongoose.Schema({
         name: String,
         dept: String
       });
       userSchema.plugin(autoIncrement.plugin, 'User');
-      var User = connection.model('User', userSchema),
+      let User = connection.model('User', userSchema),
       user1 = new User({ name: 'Charlie', dept: 'Support' }),
       user2 = new User({ name: 'Charlene', dept: 'Marketing' });;
 
-      // Act
-      async.series({
-        count1: function (cb) {
-          user1.nextCount(cb);
-        },
-        user1: function (cb) {
-          user1.save(cb);
-        },
-        count2: function (cb) {
-          user1.nextCount(cb);
-        },
-        user2: function (cb) {
-          user2.save(cb);
-        },
-        count3: function (cb) {
-          user2.nextCount(cb);
-        }
-      }, assert);
+      let results = {};
+      results.count1 = await user1.nextCount();
+      results.user1 = await user1.save();
+      results.count2 = await user1.nextCount();
+      results.user2 = await user2.save();
+      results.count3 = await user2.nextCount();
 
-      // Assert
-      function assert(err, results) {
-        should.not.exist(err);
-        results.count1.should.equal(0);
-        results.user1[0].should.have.property('_id', 0);
-        results.count2.should.equal(1);
-        results.user2[0].should.have.property('_id', 1);
-        results.count3.should.equal(2);
-        done();
-      }
+      results.count1.should.equal(0);
+      results.user1.should.have.property('_id', 0);
+      results.count2.should.equal(1);
+      results.user2.should.have.property('_id', 1);
+      results.count3.should.equal(2);
 
     });
 
-    it('resetCount should cause the count to reset as if there were no documents yet.', function (done) {
+    it('resetCount should cause the count to reset as if there were no documents yet.', async function() {
 
       // Arrange
-      var userSchema = new mongoose.Schema({
+      let userSchema = new mongoose.Schema({
         name: String,
         dept: String
       });
       userSchema.plugin(autoIncrement.plugin, 'User');
-      var User = connection.model('User', userSchema),
+      let User = connection.model('User', userSchema),
       user = new User({name: 'Charlie', dept: 'Support'});
 
-      // Act
-      async.series({
-        user: function (cb) {
-          user.save(cb);
-        },
-        count1: function (cb) {
-          user.nextCount(cb);
-        },
-        reset: function (cb) {
-          user.resetCount(cb);
-        },
-        count2: function (cb) {
-          user.nextCount(cb);
-        }
-      }, assert);
+      let results = {};
+      results.user = await user.save();
+      results.count1 = await user.nextCount();
+      results.reset = await user.resetCount();
+      results.count2 = await user.nextCount();
 
-      // Assert
-      function assert(err, results) {
-        should.not.exist(err);
-        results.user[0].should.have.property('_id', 0);
-        results.count1.should.equal(1);
-        results.reset.should.equal(0);
-        results.count2.should.equal(0);
-        done();
-      }
+      results.user.should.have.property('_id', 0);
+      results.count1.should.equal(1);
+      results.reset.should.equal(0);
+      results.count2.should.equal(0);
 
     });
 
